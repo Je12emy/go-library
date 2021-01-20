6997f8fe5617da33472defee5254aa0620cfd740
je12emy <jeremyzelayaR@gmail.com>
2021-01-20 00:27:29 -0600

    Created tests for BookRepository

--- domain/bookRepository.go
+++ domain/bookRepository.go
@@ -22,6 +22,25 @@ func (d BookRepositoryDB) FindBy(id int) (*Book, error) {
 	return &book, nil
 }
 
+// FindAll Returns a all the books with pagination, if 0 is passed no limit is imposed.
+func (d BookRepositoryDB) FindAll(limit int, offset int) ([]Book, error) {
+	books := make([]Book, 0)
+	var result *gorm.DB
+	if limit == 0 && offset == 0 {
+		result = d.dbClient.Find(&books)
+	} else {
+		result = d.dbClient.Offset(offset).Limit(limit).Find(&books)
+	}
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	// if result.RowsAffected == 0 {
+	// 	fmt.Println("book not found")
+	// }
+	return books, nil
+}
+
 // NewBookRepositoryDB Returns a new instance of BookRepository takes a gorm db client
 func NewBookRepositoryDB(dbClient *gorm.DB) BookRepositoryDB {
 	return BookRepositoryDB{dbClient}
--- domain/bookRepository_test.go
+++ domain/bookRepository_test.go
@@ -10,18 +10,44 @@ import (
 )
 
 var db *gorm.DB
+var repo domain.BookRepositoryDB
 
-func Test_should_return_a_single_book_by_its_id(t *testing.T) {
-	// Arrange
-	setup()
-	repo := domain.NewBookRepositoryDB(db)
+func setup() {
+	db = app.GetTestDBClient()
+	repo = domain.NewBookRepositoryDB(db)
+	createBooks()
+}
 
+func createBooks() {
 	db.Create(&domain.Book{
 		ID:              1,
 		Name:            "Elon Musk",
 		PublicationDate: "20/20/15",
 		Genre:           "Bibliography",
 	})
+	db.Create(&domain.Book{
+		ID:              2,
+		Name:            "The Master Algorithm",
+		PublicationDate: "20/20/15",
+		Genre:           "Science & Technology",
+	})
+	db.Create(&domain.Book{
+		ID:              3,
+		Name:            "Test Book 3",
+		PublicationDate: "20/20/15",
+		Genre:           "Science & Technology",
+	})
+	db.Create(&domain.Book{
+		ID:              4,
+		Name:            "Test Book 4",
+		PublicationDate: "20/20/15",
+		Genre:           "Science & Technology",
+	})
+}
+
+func Test_should_return_a_single_book_by_its_id(t *testing.T) {
+	// Arrange
+	setup()
 
 	// Act
 	book, err := repo.FindBy(1)
@@ -32,6 +58,36 @@ func Test_should_return_a_single_book_by_its_id(t *testing.T) {
 	}
 }
 
-func setup() {
-	db = app.GetTestDBClient()
+func Test_should_return_all_books(t *testing.T) {
+	setup()
+
+	books, err := repo.FindAll(0, 0)
+
+	if err != nil {
+		t.Error("Failed while retrieving all books")
+	}
+
+	if len(books) < 1 {
+		t.Error("Filed while retrieving the list of all books")
+	}
+}
+
+func Test_should_paginate_books(t *testing.T) {
+	setup()
+
+	booksPage1, err := repo.FindAll(2, 1)
+
+	if err != nil {
+		t.Error("Failed while retrieving page 1 of books")
+	}
+
+	booksPage2, err := repo.FindAll(2, 2)
+
+	if err != nil {
+		t.Error("Failed while retrieving page 2 of books")
+	}
+
+	if booksPage1[0].Name == booksPage2[0].Name {
+		t.Error("Failed while paginating results")
+	}
 }
