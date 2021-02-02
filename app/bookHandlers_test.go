a3a7668b2f5c8cdd8072b9a575f548e8ba8a012f
DIO <jeremyzelaya@hotmail.es>
2021-02-01 23:58:39 -0600

    Created test for create book route

--- app/bookHandlers_test.go
+++ app/bookHandlers_test.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"bytes"
+	"encoding/json"
 	"library/dto"
 	"library/mocks/service"
 	"net/http"
@@ -21,6 +23,7 @@ func setup(t *testing.T) func() {
 	bh = BookHandler{mockService}
 	router = mux.NewRouter()
 	router.HandleFunc("/books/{book_id:[0-9]+}", bh.FindBook)
+	router.HandleFunc("/books", bh.NewBook)
 
 	return func() {
 		router = nil
@@ -54,5 +57,45 @@ func Test_should_return_a_book_with_status_code_200(t *testing.T) {
 
 	if recorder.Code != http.StatusFound {
 		t.Error("Failed while testing status code")
+	}
+}
+
+func Test_should_return_a_created_book_with_status_200_ok(t *testing.T) {
+	// Arrange
+	teardown := setup(t)
+	defer teardown()
+
+	bookResponse := dto.BookResponse{
+		ID:              1,
+		Name:            "Book 1",
+		PublicationDate: "14/05/2020",
+		Genre:           "Comedy",
+	}
+
+	bookRequest := dto.BookRequest{
+		Name:            "Book 1",
+		PublicationDate: "14/05/2020",
+		Genre:           "Comedy",
+	}
+
+	message := map[string]interface{}{
+		"book_name":        "Book 1",
+		"publication_date": "14/05/2020",
+		"book_genre":       "Comedy",
+	}
+
+	bytesRepresentation, err := json.Marshal(message)
+	if err != nil {
+		t.Error("Error while creating request body")
+	}
+	mockService.EXPECT().CreateNewBook(bookRequest).Return(&bookResponse, nil)
+
+	request, _ := http.NewRequest(http.MethodPost, "/books", bytes.NewBuffer(bytesRepresentation))
+	// Act
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, request)
+	// Assert
+	if recorder.Code != http.StatusCreated {
+		t.Error("Error while validating status code")
 	}
 }
