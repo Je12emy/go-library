ad3be2f30dd178c832d303d95c6917727cde4d33
DIO <jeremyzelaya@hotmail.es>
2021-02-02 11:04:33 -0600

    Created test for delete handler

--- app/bookHandlers_test.go
+++ app/bookHandlers_test.go
@@ -22,9 +22,11 @@ func setup(t *testing.T) func() {
 	mockService = service.NewMockBookService(ctlr)
 	bh = BookHandler{mockService}
 	router = mux.NewRouter()
+
 	router.HandleFunc("/books/{book_id:[0-9]+}", bh.FindBook).Methods(http.MethodGet)
 	router.HandleFunc("/books", bh.NewBook).Methods(http.MethodPost)
 	router.HandleFunc("/books/{book_id:[0-9]+}", bh.UpdateBook).Methods(http.MethodPut)
+	router.HandleFunc("/books/{book_id:[0-9]+}", bh.DeleteBook).Methods(http.MethodDelete)
 
 	return func() {
 		router = nil
@@ -74,29 +76,17 @@ func Test_should_return_a_created_book_with_status_200_ok(t *testing.T) {
 	}
 
 	bookRequest := dto.BookRequest{
-		Name:            "Book 1",
-		PublicationDate: "14/05/2020",
-		Genre:           "Comedy",
+		ID: 1,
 	}
 
-	message := map[string]interface{}{
-		"book_name":        "Book 1",
-		"publication_date": "14/05/2020",
-		"book_genre":       "Comedy",
-	}
+	mockService.EXPECT().DeleteBook(bookRequest).Return(&bookResponse, nil)
 
-	bytesRepresentation, err := json.Marshal(message)
-	if err != nil {
-		t.Error("Error while creating request body")
-	}
-	mockService.EXPECT().CreateNewBook(bookRequest).Return(&bookResponse, nil)
-
-	request, _ := http.NewRequest(http.MethodPost, "/books", bytes.NewBuffer(bytesRepresentation))
+	request, _ := http.NewRequest(http.MethodDelete, "/books/1", nil)
 	// Act
 	recorder := httptest.NewRecorder()
 	router.ServeHTTP(recorder, request)
 	// Assert
-	if recorder.Code != http.StatusCreated {
+	if recorder.Code != http.StatusOK {
 		t.Error("Error while validating status code")
 	}
 }
@@ -141,4 +131,8 @@ func Test_should_return_the_updated_book_with_status_200_ok(t *testing.T) {
 	if recorder.Code != http.StatusOK {
 		t.Error("Error while validating status code")
 	}
+}
+
+func Test_should_return_the_deleted_book_with_status_200_ok(t *testing.T) {
+
 }
